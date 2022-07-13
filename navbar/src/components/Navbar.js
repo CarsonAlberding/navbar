817a4f736330ccca4fc8ffc43dcff1b4c1ac510a
CarsonAlberding <carson.alberding@cdpipelinedevshop.com>
2022-07-13 14:25:25 +0000

    commit 2

--- navbar/src/components/Navbar.js
+++ navbar/src/components/Navbar.js
@@ -8,97 +8,95 @@ import DashboardIcon from "@mui/icons-material/Dashboard";
 import FriendsIcon from "@mui/icons-material/People";
 import ImagesIcon from "@mui/icons-material/Image";
 
-const ulStyle={
-    listStyleType:'none'
-  }
-
-const paperStyle={
-    backgroundColor: "grey",
-    padding: "20px",
-    height: "70vh",
-    width: 420,
-    margin: "20px auto",
-    borderRadius : 20
-}
-
 const navbarData = [
     {
         title: "Home",
-        icon: <HomeIcon />,
-        margin:"10px 0",
-        link: "/home"
+        icon: <HomeIcon />,  
+        link: "/home",
+        
+
         
     },
 
     {
         title: "Mail",
-        icon: <MailIcon />,
-        margin:"10px 0",
+        icon: <MailIcon />,   
         link: "/mail"
     },
 
     {
         title: "Analytics",
-        icon: <AnalyticsIcon />,
-        margin:"10px 0",
+        icon: <AnalyticsIcon />,  
         link: "/analytics"
     },
 
     {
         title: "Dashboard",
-        icon: <DashboardIcon />,
-        margin:"10px 0",
+        icon: <DashboardIcon />,    
         link: "/dashboard"   
     },
         
     {
         title: "Friends",
         icon: <FriendsIcon />,
-        margin:"10px 0",
         link: "/friends"
     },
 
     {
         title: "Images",
         icon: <ImagesIcon />,
-        margin:"10px 0",
         link: "/images"
     }
 
 ]
 
+const ulStyle={
+    listStyleType: 'none'
+}
 
-    
+const paperStyle={
+    backgroundColor: "#2b9ff3",
+    padding: "5px",
+    height: "70vh",
+    width: 170,
+    margin: "20px auto",
+    borderRadius : 20
+}
+
+const textstyle= {
+    padding: "10px"
+}
 
 function Navbar(){
     return(
-        <Paper 
-        style={paperStyle}>
+        <Paper style={paperStyle}>
             <div className="Navbar">
-                <ul style={ulStyle} className="NavbarList">
-                    {navbarData.map((val,key)=> {
-                        return(
-                            <li key ={key}
-                                className="row"
-                                onClick={() => {window.location.pathname = val.link}}>
-                                    <p></p>
-                                    {" "}
-                                    <div id="icon" align= "left">{val.icon}</div>{" "}
-                                    <div id="title" align = "left">
-                                        {val.title}
-                                    </div>
-                                </li>
-                        )
+            <ul style={ulStyle} className="NavbarList">
+            {navbarData.map((val,key)=> {
+                return(
+                <li key ={key}
+                    className="row"
+                    onClick={() => {window.location.pathname = val.link}}>
+                        <p></p>
+                        {" "}
+                        <div id="icon" align= "center">{val.icon}</div>{" "}
+                        <div id="title" align = "center" style={textstyle}>
+                            {val.title}
+                        </div>
+                </li>
+                )
 
 
 
-                    })}
-                </ul>
+            })}
+            </ul>
 
             </div>
         </Paper>
     )
 }
+
+
 
 
 export default Navbar;
